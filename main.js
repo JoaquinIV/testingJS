f79032a4bfe8666b5fa9c0360e0ff05bf4d191fa
Ignacio <ignaciovlnt@gmail.com>
2022-06-22 20:25:50 -0300

    Métodos TransaccionPago ignacio

---
+++ main.js
@@ -0,0 +1,27 @@
+// from datetime import datetime
+
+class TransaccionPago{
+    
+    constructor(nombreTitularTransaccion,digitosTarjetaTransaccion,estadoTransaccion){
+        this.nombreTitularTransaccion = nombreTitularTransaccion;
+        this.digitosTarjetaTransaccion = digitosTarjetaTransaccion;
+        this.fechaActualTransacción = Date.now();
+        this.estadoTransaccion = estadoTransaccion;     
+    }
+
+    //Nacho
+    esNombreTitularString(){
+        return typeof(this.nombreTitularTransaccion);
+    }
+    esNombreCadenaVacia(){
+        return this.nombreTitularTransaccion.length == 0;
+    }
+    setFechaActual(){
+        return this.fechaActualTransacción;
+    }
+    
+
+}
+
+export {TransaccionPago}
+

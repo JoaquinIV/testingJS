1fb7499da0246cf0f620379fa23f6518964150d9
Ignacio <ignaciovlnt@gmail.com>
2022-06-22 23:03:49 -0300

    Test unitarios agregados sara

--- test_js.js
+++ test_js.js
@@ -21,17 +21,24 @@ describe("Pruebas unitarias TransaccionPago", () => {
             assert.equal(transaccionPrueba.setFechaActual(), Date.now());
         })
 
+        it("Verificamos que nos retorne el nombre del titular", () =>{
+            let transaccionPrueba = new TransaccionPago("Pedro Fernandez", 1234, "En espera");
+            assert.equal(transaccionPrueba.nombreTitular(), "Pedro");
+        })
+
+    })
+
+    describe("Pruebas sara", () => {
+        
+        it("Verificamos si el titular de la tarjeta es una variable string alfabética", () =>{
+            let transaccionPrueba = new TransaccionPago("Victor Nomberto", 1234, "En espera");
+            assert.equal(transaccionPrueba.esNombreAlfabetico(), true);
+        })
+
+        it("Verificamos si el nro de tarjeta contiene 4 digitos", () =>{
+            let transaccionPrueba = new TransaccionPago("Victor Nomberto", 1234, "En espera");
+            assert.equal(transaccionPrueba.nroDigitosTarjetaTransaccion(), 4);
+        })
+
     })
 })
-
-// #NACHO
-//     def test_titularTarjetaString(self):
-//         self.assertTrue(TransaccionPago("Pedro Fernandez", 1234, "En espera").esNombreTitularString())
-
-//     #NACHO
-//     def test_titularTarjetaCadenaVacia(self):
-//         self.assertFalse(TransaccionPago("Pedro Fernandez", 1234, "En espera").esNombreCadenaVacia())
-
-//     #NACHO
-//     def test_fechaActualTransaccion(self):
-//         self.assertEqual(TransaccionPago("Pedro Fernandez", 1234, "En espera").fechaActualTransaccion, datetime.today())
